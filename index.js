9259c7d8be80936d99de6edbec5ea68a5902a76f
Simran <simran.maurya_cs18@gla.ac.in>
2021-04-22 21:32:59 +0530

    handle routes of different requests

--- index.js
+++ index.js
@@ -12,3 +12,24 @@ const PORT= process.env.PORT || 4000;
 app.listen(4000, function(){
     console.log(`Server started at PORT ${PORT}`);
 });
+//nodemon automatically restarts server by windows powershell
+//OR
+// goto package.json and add "start":"nodemon index.js" in scripts
+
+// Handle Client Server
+
+//Syntax
+//get, post, put, delete
+// app.method('Route',callback(){})
+
+// Handle '/' Home Route
+
+app.get('/',function(req,res){
+    res.send("Hello from Express server");
+});
+
+app.get('/about',function(req,res){
+    res.send("Welcome to About Page");
+})
+
+
