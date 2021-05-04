4c32c116dfabef3ee60fc7c66f26f31cc1f7d926
Simran <simran.maurya_cs18@gla.ac.in>
2021-05-04 20:06:50 +0530

    PUT and DELETE request api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Require your express module
 const express =require("express");
-
+const joi= require("@hapi/joi");
+const Joi = require("@hapi/joi");
 //Initialize your express app
 const app= express();
 
@@ -62,14 +63,76 @@ app.get('/api/courses/:name', (req,res)=>{
 
 // API for creating new course
 //use middleware
-app.use(express.json());
+/*
+app.use(express.json()); //converts data into json format
 app.post('/api/courses', (req,res)=>{
     const course= {
         id:courses.length +1,
         name :req.body.name,
     };
 
-    courses.push(course);
+    courses.push(course);  //push new course into previous courses array
     // res.send(course)
     res.send(course);
+});
+*/
+
+//API for handling PUT request and UPDATE the course
+
+app.put('/api/courses/:id', (req,res) =>{
+    const course= courses.find((c)=> c.id === parseInt(req.params.id))
+    if(!course) 
+        res.status(404).send("The course you are looking is not available");
+   //checks if course is available or not
+
+    //Validate the body data
+    const schema= Joi.object(
+        {
+            name: Joi.string().min(3).required()
+        }
+    );
+    const result =schema.validate(req.body);
+    if(result.error){
+    res.status(404).send(result.error.message);
+    return;
+    }
+    course.name = req.body.name;
+    res.send("Course updated successfully");
+});
+
+app.use(express.json());
+app.post('/api/courses', (req,res) =>{
+    //Validate the body data
+    const schema= Joi.object(
+        {
+            name: Joi.string().min(3).required()
+        }
+    );
+    const result =schema.validate(req.body);
+    if(result.error){
+    res.status(404).send(result.error.message);
+    return;
+    }
+    const course= {
+        id:courses.length +1,
+        name :req.body.name,
+    };
+
+    courses.push(course);  //push new course into previous courses array
+    res.send("New course added");
+});
+
+//API for handling delete request and it will delete the course
+
+app.delete('/api/courses/:id', (req,res)=>{
+    const course= courses.find((c)=> c.id === parseInt(req.params.id))
+    if(!course) 
+        res.status(404).send("The course you are looking is not available");
+   //checks if course is available or not
+   
+   //Delete the course with the index
+   //indexOf gives index of array element
+   const index= courses.indexOf(course);
+   courses.splice(index,1) ; //we need to delete course at index position and only one couse deletion
+   res.send("Course deleted successfully");
 });
